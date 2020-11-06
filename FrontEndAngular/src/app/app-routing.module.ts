b952dae946c636ebff1daf937abfdd105f99f3cb
unknown <mila_aandrade@hotmail.com>
2020-11-06 11:56:15 -0300

    devs incompleto - wip

--- FrontEndAngular/src/app/app-routing.module.ts
+++ FrontEndAngular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {SobrenosComponent} from './sobrenos/sobrenos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path:'home', component: HomeComponent},
+  {path:'sobrenos', component: SobrenosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
