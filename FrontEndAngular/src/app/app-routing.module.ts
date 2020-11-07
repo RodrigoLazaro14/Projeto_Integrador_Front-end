fde063fc31e8eef18b5ebfdcbd069caf63dba5ad
Rebecca <rebecca_medeiros_ramaldes@hotmail.com>
2020-11-07 16:17:35 -0300

    07-11-2020

--- FrontEndAngular/src/app/app-routing.module.ts
+++ FrontEndAngular/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {SobrenosComponent} from './sobrenos/sobrenos.component';
 import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'home', pathMatch: 'full'},
   {path:'home', component: HomeComponent},
   {path:'sobrenos', component: SobrenosComponent},
-  { path:'login', component:LoginComponent }
+  {path:'logar', component:LoginComponent },
+  {path:'cadastrar', component:CadastroComponent }
 ];
 
 @NgModule({
--- FrontEndAngular/src/app/cadastro/cadastro.component.ts
+++ FrontEndAngular/src/app/cadastro/cadastro.component.ts
@@ -19,6 +19,8 @@ export class CadastroComponent implements OnInit {
     
   ) { }
 
+ 
+
   ngOnInit() {
   }
 
