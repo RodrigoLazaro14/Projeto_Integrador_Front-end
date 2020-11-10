227e4d75fdc4ab33d93c8269e646e46c50b8f4c0
unknown <mila_aandrade@hotmail.com>
2020-11-10 18:03:54 -0300

    botao direcionamento - wip

--- FrontEndAngular/src/app/app.module.ts
+++ FrontEndAngular/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SobrenosComponent } from './sobrenos/sobrenos.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { DirecionamentoComponent } from './direcionamento/direcionamento.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     FooterComponent,
     LoginComponent,
     CadastroComponent,
-    SobrenosComponent
+    SobrenosComponent,
+    DirecionamentoComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontEndAngular/src/app/direcionamento/direcionamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-direcionamento',
+  templateUrl: './direcionamento.component.html',
+  styleUrls: ['./direcionamento.component.css']
+})
+export class DirecionamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
