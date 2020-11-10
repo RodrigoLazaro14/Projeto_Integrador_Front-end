00e3ed6bc30ef93d570f55dc361d654ddfb991d0
LeandroSilvaPaiva <70709526+LeandroSilvaPaiva@users.noreply.github.com>
2020-11-10 15:52:50 -0300

    Especialidades

--- FrontEndAngular/src/app/app-routing.module.ts
+++ FrontEndAngular/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import {SobrenosComponent} from './sobrenos/sobrenos.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { EspecialidadesComponent } from './especialidades/especialidades.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'home', pathMatch: 'full'},
   {path:'home', component: HomeComponent},
   {path:'sobrenos', component: SobrenosComponent},
   {path:'logar', component:LoginComponent },
-  {path:'cadastrar', component:CadastroComponent }
+  {path:'cadastrar', component:CadastroComponent },
+  {path:'especialidades', component:EspecialidadesComponent}
 ];
 
 @NgModule({
