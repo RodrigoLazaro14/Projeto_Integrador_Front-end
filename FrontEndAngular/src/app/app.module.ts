a547339c16141cc8aaef047e72969860b52f00f4
unknown <mila_aandrade@hotmail.com>
2020-11-10 20:31:26 -0300

    direcionamento cadastro wip

--- FrontEndAngular/src/app/app.module.ts
+++ FrontEndAngular/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,7 +12,7 @@ import { SobrenosComponent } from './sobrenos/sobrenos.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { DirecionamentoComponent } from './direcionamento/direcionamento.component';
-
+import { EspecialidadesComponent } from './especialidades/especialidades.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +23,8 @@ import { DirecionamentoComponent } from './direcionamento/direcionamento.compone
     LoginComponent,
     CadastroComponent,
     SobrenosComponent,
-    DirecionamentoComponent
+    DirecionamentoComponent,
+    EspecialidadesComponent
   ],
   imports: [
     BrowserModule,
--- FrontEndAngular/src/app/direcionamento/direcionamento.component.ts
+++ FrontEndAngular/src/app/direcionamento/direcionamento.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-direcionamento',
   templateUrl: './direcionamento.component.html',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirecionamentoComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
