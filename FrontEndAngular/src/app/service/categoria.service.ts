cfb38118519fe5fda3e29ba05aac9b69091f8efe
unknown <mila_aandrade@hotmail.com>
2020-11-18 11:06:40 -0300

    18/11

--- FrontEndAngular/src/app/service/categoria.service.ts
+++ FrontEndAngular/src/app/service/categoria.service.ts
@@ -12,13 +12,13 @@ export class CategoriaService {
     headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
   }
   getAllCategorias() {
-    return this.http.get('http://localhost:8080/pesquisa-hospitais', this.token)
+    return this.http.get('http://localhost:8080/categoria', this.token)
   }
-  getByIdCategoria(id: number){
-    return this.http.get(`http://localhost:8080/pesquisa-hospitais/${id}`,this.token)
+  getByIdCategoria(idCategoria: number){
+    return this.http.get(`http://localhost:8080/categoria/${idCategoria}`,this.token)
   }
  
   postCategoria(categoria: CategoriaModel){
-    return this.http.post('http://localhost:8080/especialidades', categoria, this.token)
+    return this.http.post('http://localhost:8080/categoria', categoria, this.token)
   }
 }
--- FrontEndAngular/src/app/service/hospital.service.ts
+++ FrontEndAngular/src/app/service/hospital.service.ts
@@ -21,7 +21,9 @@ export class HospitalService {
   getByIdHospital(id: number) {
     return this.http.get(`http://localhost:8080/pesquisa-hospitais/${id}`, this.token)
   }
-
+  getByIdHospital2(id: number) {
+    return this.http.get(`http://localhost:8080/hospitais/${id}`, this.token)
+  }
 
   getByNomeHospital(nome: string) {
     return this.http.get(`http://localhost:8080/pesquisa-hospitais/nome/${nome}`, this.token)
